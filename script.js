b63c9c4db54800c3d470491684b16489ddcece9c
Rachel Thompson <thompson.1657@gmail.com>
2020-11-20 16:26:17 -0600

    working with console.log(password)

--- script.js
+++ script.js
@@ -13,11 +13,10 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 var numericNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var specialChar = ['~','!','@','#','$','%','^','&','*','(',')','_','+','-','=','?']
-
 
 var charCount = parseInt(prompt('Pick a password length of 8-128 characters'))
 
@@ -37,7 +36,26 @@ if(includeNumbers){
     finalChars.push(numericNumber[i])
   }
 
-  if(includeSpecial){
+if(includeSpecial){
     finalChars.push(specialChar)
   }
+
+if(includeUpper){
+    finalChars.push(uppercase)
 }
+
+if(includeLower){
+  finalChars.push(lowercase)
+}
+
+var flattenedFinalChars = finalChars.flat()
+var password=''
+
+for (var i=0; i<charCount; i++){
+  var randomPass= Math.floor( Math.random()* flattenedFinalChars.length)
+  var randomChar= flattenedFinalChars[randomPass]
+  password += randomChar
+}
+}
+console.log(password)
+//generateBtn.addEventListener(click,)
